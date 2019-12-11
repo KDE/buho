50ebd83e78c47ddcc9f3511bd4698dd5c7f14ed6
Nicolas Fella <nicolas.fella@gmx.de>
2019-12-11 14:51:40 +0100

    Add missing files

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+function parse(html, query)
+{
+    parser=new DOMParser();
+    htmlDoc=parser.parseFromString(txt, "text/html");
+    // do whatever you want with htmlDoc.getElementsByTagName('a');
+}
